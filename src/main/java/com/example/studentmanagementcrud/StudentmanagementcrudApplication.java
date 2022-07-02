744ea2ae57a658a761372a611acf9092e629df1b
Cavin Karema <cavin.mulewa.karema@gmail.com>
2022-07-03 01:20:02 +0300

    Initial commit

---
+++ src/main/java/com/example/studentmanagementcrud/StudentmanagementcrudApplication.java
@@ -0,0 +1,13 @@
+package com.example.studentmanagementcrud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudentmanagementcrudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudentmanagementcrudApplication.class, args);
+	}
+
+}
